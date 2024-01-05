fada8caeab58f9c76ec7db96b664f8b6c891092d
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2024-01-05 00:10:49 -0300

    improve the collect of the data to make sense with the shape of the train of the model

--- create_dataset.py
+++ create_dataset.py
@@ -3,7 +3,7 @@ import shutil
 import cv2
 import pickle
 import mediapipe as mp
-import time
+# import time
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -22,11 +22,13 @@ if not os.path.exists('./data'):
 dataset_size = 100
 signals = [
     'A',
-    'B',
-    'C',
-    'D',
-    'E'
+    'E',
+    'I',
+    'O',
+    'U'
 ]
+data = []
+labels = []
 
 print(f'Collecting images')
 
@@ -54,53 +56,31 @@ for index, signal in enumerate(signals):
     if cv2.waitKey(25) == ord('q'):
       break
 
-  time.sleep(1)
+  # time.sleep(1)
 
   counter = 0
   while counter < dataset_size:
     ret, frame = cap.read()
     cv2.imshow('frame', frame)
-    cv2.waitKey(25)
-    cv2.imwrite(os.path.join('./data', str(
-        index), '{}.jpg'.format(counter)), frame)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(frame_rgb)
+
+    coordinates = []
+
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        for landmark in hand_landmarks.landmark:
+          coordinates.append([landmark.x, landmark.y])
+
+      data.append(coordinates)
+      labels.append(index)
 
     counter += 1
+    cv2.waitKey(25)
 
 cap.release()
 cv2.destroyAllWindows()
-
-print(f'Collecting landmarks')
-
-data = []
-labels = []
-for dir_ in os.listdir('./data'):
-  for img_path in os.listdir(os.path.join('./data', dir_)):
-    data_aux = []
-
-    x_ = []
-    y_ = []
-
-    img = cv2.imread(os.path.join('./data', dir_, img_path))
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    results = hands.process(img_rgb)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        for i in range(len(hand_landmarks.landmark)):
-          x = hand_landmarks.landmark[i].x
-          y = hand_landmarks.landmark[i].y
-
-          x_.append(x)
-          y_.append(y)
-
-        for i in range(len(hand_landmarks.landmark)):
-          x = hand_landmarks.landmark[i].x
-          y = hand_landmarks.landmark[i].y
-          data_aux.append(x - min(x_))
-          data_aux.append(y - min(y_))
-
-      data.append(data_aux)
-      labels.append(dir_)
 
 f = open('data.pickle', 'wb')
 pickle.dump({'data': data, 'labels': labels}, f)
