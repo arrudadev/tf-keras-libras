8e33dfef3721efc5734bae9d7a01734de2bb7801
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2024-01-04 21:37:35 -0300

    change the train of the model to use tensorflow and keras

--- train_model.py
+++ train_model.py
@@ -1,27 +1,41 @@
 import pickle
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+# Carregar os dados
 data_dict = pickle.load(open('./data.pickle', 'rb'))
+data = np.array(data_dict['data'], dtype=np.int32)
+labels = np.array(data_dict['labels'], dtype=np.int32)
 
-data = np.array(data_dict['data'])
-labels = np.array(data_dict['labels'])
+print(labels)
 
+# Dividir os dados em conjuntos de treino e teste
 x_train, x_test, y_train, y_test = train_test_split(
     data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
-model = RandomForestClassifier()
+# Definir a arquitetura da rede neural
+model = tf.keras.Sequential([
+    # A entrada agora é o número de dimensões dos landmarks
+    tf.keras.layers.Dense(64, activation='relu', input_shape=(len(data[0]),)),
+    tf.keras.layers.Dense(32, activation='relu'),
+    # Camada de saída com ativação softmax para classificação multiclasses
+    tf.keras.layers.Dense(np.max(labels) + 1, activation='softmax')
+])
 
-model.fit(x_train, y_train)
+# Compilar o modelo
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-y_predict = model.predict(x_test)
+# Treinar o modelo
+model.fit(x_train, y_train, epochs=10, batch_size=32, validation_split=0.1)
 
-score = accuracy_score(y_predict, y_test)
+# Avaliar o modelo no conjunto de teste
+y_predict = np.argmax(model.predict(x_test), axis=1)
+score = accuracy_score(y_test, y_predict)
 
-print('{}% of samples were classified correctly !'.format(score * 100))
+print('{}% das amostras foram classificadas corretamente!'.format(score * 100))
 
-f = open('model.p', 'wb')
-pickle.dump({'model': model}, f)
-f.close()
+# Salvar o modelo
+model.save('model.h5')
